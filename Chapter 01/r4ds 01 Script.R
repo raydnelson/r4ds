e89ded0fb3ad09c12d966c3e39f497ed9e22aafa
Ray Nelson <raydnelson@live.com>
2018-05-17 16:59:54 -0600

    r4ds Chapters 1 - 3
    Book scripts

---
+++ Chapter 01/r4ds 01 Script.R
@@ -0,0 +1,22 @@
+# R for Data Science
+# Initial: May 17, 2018
+# Revision: May 18, 2018
+# Ray Nelson
+
+# Basic tidyverse library
+
+install.packages("tidyverse")
+library(tidyverse)
+
+# Other packages
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+# 1.5 Running R Code
+1 + 2
+
+# Check on whether tidyverse is up to date
+tidyverse_update()
+
+# reprex
+# packages at the top if the script
+# Includ the data using dput()
---
+++ Chapter 03/r4ds Chapter 03 Script.R
@@ -0,0 +1,6 @@
+# R for Data Science Chapter 03
+# Initial: May 17, 2018
+# Revision: May 18, 2018
+# Ray Nelson
+
+library(tidyverse)
