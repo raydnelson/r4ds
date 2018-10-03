d06781af23273f1baf3d78cfc73b0b44db272042
Ray Nelson <raydnelson@live.com>
2018-10-03 17:02:12 -0600

    New Menu
    Lecture Updates

--- Chapter 07/Variation Outline.R
+++ Chapter 07/Variation Outline.R
@@ -12,21 +12,16 @@ diamonds %>% summary()
 diamonds %>% View()
 ? diamonds
 
-# Variation in categorical variables
+# Count and Graph Variation in categorical variables
 ## Cut
 diamonds %>% count(cut)
 diamonds %>% ggplot(mapping = aes(x = cut)) +
   geom_bar(fill = "hotpink")
 
 ## Color
-diamonds %>% count(color)
-diamonds %>% ggplot(mapping = aes(x = color)) +
-  geom_bar(fill = "lightblue")
+
 
 ## Clarity
-diamonds %>% count(clarity)
-diamonds %>% ggplot(mapping = aes(x = clarity)) +
-  geom_bar(fill = "lightgreen")
 
 # Function to look at categorical variables
 categorical <- function(characteristic) {
@@ -41,71 +36,35 @@ categorical(characteristic = "clarity")
 
 # Size of diamonds
 ## Histogram
-diamonds %>% ggplot(mapping = aes(x = carat)) +
-  geom_histogram()
+
 
 # Smaller diamonds and the size of the bin width
 ## Histogram
-diamonds %>% filter(carat < 3) %>%
-  ggplot(mapping = aes(x = carat)) +
-    geom_histogram(binwidth = 0.01, fill = "lightgreen")
+
 
 # Diamond size with freqpoly
 ## Histogram and freqpoly
 ## All diamonds
-diamonds %>%
-  ggplot(mapping = aes(x = carat)) +
-    geom_histogram() +
-    geom_freqpoly(color = "red", size = 2)
+
 
 # Diamonds less than 3 carats
-diamonds %>% filter(carat < 3) %>%
-  ggplot(mapping = aes(x = carat)) +
-  geom_freqpoly(color = "blue")
+
 
 # Rug Plots
 ## Histogram and rug plots
-diamonds %>% 
-  ggplot(mapping = aes(x = carat)) +
-    geom_histogram(fill = "lightblue") +
-    geom_rug()
+
 
 # all diamonds density and the influence of adjust
-diamonds %>%
-  ggplot(mapping = aes(x = carat)) +
-    geom_density(adjust = 0.5, fill = "lightblue")
+
 
 # all diamonds with boxplot
-diamonds %>% 
-  ggplot(mapping = aes(x = factor(0), y = carat)) +
-    geom_boxplot() +
-    coord_flip()
+
 
 # violin plot
-diamonds %>% 
-  ggplot(mapping = aes(x = factor(0), y = carat)) +
-    geom_violin(adjust = 3, fill = "lightblue") +
-    geom_boxplot(width = 0.25, fill = "lightgreen") +
-    coord_flip() +
-    labs(
-      title = "Violin Plot of Diamonds Weight",
-      x = "",
-      y = "Weight in Carats"
-    )
+
 
 # Violin plot for smaller diamonds 2 carats or less
-diamonds %>% 
-  filter(carat <= 2) %>% 
-  ggplot(mapping = aes(x = factor(0), y = carat)) +
-    geom_violin(adjust = 2, fill = "lightblue") +
-    geom_boxplot(width = 0.25, fill = "lightgreen") +
-    coord_flip() +
-    labs(
-      title = "Violin Plot of Diamonds Weight",
-      subtitle = "Smaller Diamonds, Two Carats or Less",
-      x = "",
-      y = "Weight in Carats"
-  )
+
 
 # Old Faithful distribution of eruptions
 ## Information and faithful dataset
@@ -122,30 +81,17 @@ faithful %>%
   geom_rug()
 
 ## Freqpoly with rug plot
-faithful %>%
-  ggplot(mapping = aes(x = eruptions)) +
-  geom_freqpoly() +
-  geom_rug()
+
 
 ## Density Trace and rug
-faithful %>%
-  ggplot(mapping = aes(x = eruptions)) +
-  geom_density(fill = "hotpink")+
-  geom_rug()
+
 
 ## Boxplot
-faithful %>%
-  ggplot(mapping = aes(x = factor(0), y = eruptions)) +
-  geom_boxplot(fill = "lightgreen") +
-  coord_flip()
+
 
 ## Violin plot
 
-faithful %>%
-  ggplot(mapping = aes(x = factor(0), y = eruptions)) +
-  geom_violin(fill = "lightblue") +
-  geom_boxplot(width = 0.05, fill = "lightgreen") +
-  coord_flip()
+
 
 # Missing values
 ## Histogram of y with all observations
@@ -166,7 +112,6 @@ diamonds2 <- diamonds %>%
 ## Turn strange values into missing values
 diamonds2  <-  diamonds %>% 
   mutate(y = ifelse(y < 3 | y > 20, NA, y))
-
 
 # Removing missing values
 diamonds2 %>% 
