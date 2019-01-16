819f06f3e75edb254f4389f84bca331e58155b4b
Ray Nelson <raydnelson@live.com>
2019-01-15 17:02:53 -0700

    ggplot geoms
    Preparation for lecture

--- Chapter 03/Problem Set.R
+++ Chapter 03/Problem Set.R
@@ -6,13 +6,21 @@
 # Libraries
 library(tidyverse)
 
+data(mpg)
+
 # Code to interpret
 
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = class)) +
   geom_point(position = "jitter", show.legend = FALSE) +
-  geom_smooth(method = "lm", se = FALSE, show.legend = FALSE, color = "black", size = 0.50) +
+  geom_smooth(method = "lm", se = FALSE, show.legend = FALSE,
+              color = "black",
+              size = 0.50) +
   geom_smooth(se = FALSE, show.legend = FALSE) +
   facet_wrap(vars(class))
 
 # Code to write
 
+# Bar Chart of Number of cy
+ggplot(data = mpg, mapping = aes(x = class, fill = drv)) +
+  geom_bar(position = "fill") +
+  facet_grid(rows = vars(year))
