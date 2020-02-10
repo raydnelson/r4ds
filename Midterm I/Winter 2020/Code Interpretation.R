4459cba0d3adfba6ed135632aaab723991cada07
Ray Nelson <raydnelson@live.com>
2020-02-10 16:46:45 -0700

    midterm
    updates

---
+++ Midterm I/Winter 2020/Code Interpretation.R
@@ -0,0 +1,45 @@
+# Midterm I
+# Initial: 10 Feb 2020
+# Revision: 10 Feb 2020
+# Ray Nelson
+
+# Libraries
+library(tidyverse)
+library(nycflights13)
+
+# 
+flights %>% 
+  filter(!is.na(dep_delay)) %>% 
+  filter(!is.na(arr_delay)) %>% 
+  filter(origin == "JFK") %>% 
+  filter(dest %in% c("ATL", "LAX", "ORD")) %>% 
+  select(dest, dep_delay)  %>% 
+  group_by(dest) %>% 
+  summarize(departure_delay = mean(dep_delay, na.rm = TRUE))
+
+# Proportion
+flights %>% 
+  group_by(origin) %>% 
+  summarize(proportion = mean(dep_delay < 0, na.rm = TRUE)) %>% 
+  ggplot(aes(x =  origin, y = proportion, fill = origin)) +
+  geom_col(show.legend = FALSE)
+
+# Integer arithmetic
+flights %>% 
+  transmute(origin, hour = arr_time %/% 100)  %>% 
+  ggplot(aes(x = hour, fill = origin)) +
+  geom_bar(show.legend = FALSE) +
+  facet_grid(rows = "origin")
+
+flights %>% 
+  filter(dep_delay < 60) %>% 
+  ggplot(aes(x = fct_reorder(origin, dep_delay, median), y = dep_delay)) +
+  geom_violin(fill = "lightblue") +
+  geom_boxplot(width = 0.1, fill = "grey60") +
+  coord_flip()
+
+flights %>%
+  group_by(origin) %>% 
+  summarize(destinations = n_distinct(dest)) %>% 
+  arrange(destinations %>%  desc())
+
